f858c96ff78e993d8bd0a973ccb638def8e8e54f
Alexandre Cirilo <38657258+alxdrcirilo@users.noreply.github.com>
2024-01-08 22:11:10 +0100

    feat: add apple-touch-icon favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  head: {
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "128x128",
+        href: "apple-icon.png",
+      }
+    ]
+  },
   css: [
     '~/assets/css/main.css',
   ],
